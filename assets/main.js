f03c9f1df5c9e85fc98e05297d24ac272085e70d
CreateYourReality <timo.moewes@icloud.com>
2023-05-05 12:45:51 +0200

    final changes js

--- assets/main.js
+++ assets/main.js
@@ -1,8 +1,8 @@
-const koerpergroesse = document.querySelector("#size"); 
-const alter = document.querySelector("#age");
-const gewicht = document.querySelector("#weight");
-const geschlecht = document.querySelector("#male"); 
-const aktivitaet = document.querySelector("#actions");
+const koerpergroesseFeld = document.querySelector("#size"); 
+const alterFeld = document.querySelector("#age");
+const gewichtFeld = document.querySelector("#weight");
+const geschlechtFeld = document.querySelector("#male"); 
+const aktivitaetFeld = document.querySelector("#actions");
 
 const outputGrundKCAL = document.querySelector(".outputGrundKCAL");
 const outputGrundKJ = document.querySelector(".outputGrundKJ");
@@ -14,33 +14,23 @@ const femaleConst = 655.1;
 const KJkonstante = 4.184;
 
 
-//Ersetzt Punkt und Komma in einem String
+//Ersetzt Punkt und Komma in einem String (1,74m || 1.74m => 174cm)
 const replaceSymbols = (symbol) => {
-    if(symbol.includes("."))
+    if(symbol.includes(".") || symbol.includes(",")){
         symbol = symbol.replace(".","");
-
-    if(symbol.includes(","))
         symbol = symbol.replace(",","");  
-
+    }
     return Number(symbol);        
-}
-
-//Ersetzt ein Komma durch einen Punkt in einem String
-const replaceKomma = (gewicht) =>{
-    if(gewicht.includes(","))
-        gewicht = gewicht.replace(",",".");
-
-  return Number(gewicht);
 }
 
 
 const calcKalos = () => {
-    let size = replaceSymbols(koerpergroesse.value);
-    let age = Number(alter.value);
-    let weight = replaceKomma(gewicht.value);
+    let size = replaceSymbols(koerpergroesseFeld.value);
+    let age = Number(alterFeld.value);
+    let weight = Number(gewichtFeld.value);
     let gender = true;
-    geschlecht.checked ? gender = true : gender = false; //true = checkbox male || false = checkbox female
-    let action = Number(aktivitaet.value); // 0.95 | 1.2 | 1.5 | 1.7 | 1.9 | 2.2
+    geschlechtFeld.checked ? gender = true : gender = false; //true = checkbox male || false = checkbox female
+    let action = Number(aktivitaetFeld.value); // 0.95 | 1.2 | 1.5 | 1.7 | 1.9 | 2.2
 
     if(isNaN(size) || isNaN(weight)){ //wenn size oder weight keine Zahlen sind, brech ab
         outputGrundKCAL.innerHTML = "Bitte gibt gültige Zahlen ein";
