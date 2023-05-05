0b899f4d0f05cfea4460a582b279a7e082f72154
CreateYourReality <timo.moewes@icloud.com>
2023-05-05 11:21:34 +0200

    working

---
+++ assets/main.js
@@ -0,0 +1,77 @@
+const koerpergroesse = document.querySelector("#size"); 
+const alter = document.querySelector("#age");
+const gewicht = document.querySelector("#weight");
+const geschlecht = document.querySelector("#male"); 
+const aktivitaet = document.querySelector("#actions");
+
+const outputGrundKCAL = document.querySelector(".outputGrundKCAL");
+const outputGrundKJ = document.querySelector(".outputGrundKJ");
+const outputGesamtKCAL = document.querySelector(".outputGesamtKCAL");
+const outputGesamtKJ = document.querySelector(".outputGesamtKJ");
+
+const maleConst = 66.47;
+const femaleConst = 655.1;
+const KJkonstante = 4.184;
+
+
+const replaceSymbols = (symbol) => {
+    if(Number(symbol) == NaN){
+        console(symbol);
+        console.log("Das ist keine Zahl!");
+        return;
+    }
+
+    if(symbol.includes("."))
+        symbol = symbol.replace(".","");
+
+    if(symbol.includes(","))
+        symbol = symbol.replace(",","");  
+
+    return Number(symbol);        
+}
+
+
+const replaceKomma = (gewicht) =>{
+    if(Number(gewicht) == NaN){
+        console(gewicht);
+        console.log("Das ist keine Zahl!");
+        return;
+    }
+
+    if(gewicht.includes(","))
+        gewicht = gewicht.replace(",",".");
+
+  return Number(gewicht);
+}
+
+
+const calcKalos = () => {
+    let size = replaceSymbols(koerpergroesse.value);
+    let age = alter.value;
+    let weight = replaceKomma(gewicht.value);
+    let gender = true;
+    geschlecht.checked ? gender = true : gender = false; //true male
+    let action = aktivitaet.value;
+
+    let gesamtKCAL;
+    let gesamtKJ;
+    let grundKCAL;
+    let grundKJ;
+
+    if(gender){ //male
+        grundKCAL = (maleConst + (13.7 * weight) + (5* size ) - (6.8 * age)).toFixed(2);
+        grundKJ = (grundKCAL * KJkonstante).toFixed(2);
+        gesamtKCAL = (grundKCAL * action).toFixed(2);
+        gesamtKJ = (gesamtKCAL * KJkonstante).toFixed(2);
+    } else{ //female
+        grundKCAL = (femaleConst + (9.6 * weight) + (1.8 * size ) - (4.7 * age)).toFixed(2);
+        grundKJ = (grundKCAL * KJkonstante).toFixed(2);
+        gesamtKCAL = (grundKCAL * action).toFixed(2);
+        gesamtKJ = (gesamtKCAL * KJkonstante).toFixed(2);
+    }
+
+    outputGrundKCAL.innerHTML = grundKCAL + " KCAL";
+    outputGrundKJ.innerHTML = grundKJ + " KJ";
+    outputGesamtKCAL.innerHTML = gesamtKCAL + " KCAL";
+    outputGesamtKJ.innerHTML = gesamtKJ + " KJ";
+}
